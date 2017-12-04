477800d0866927de69e7f2ab5319829d95d28af7
tkukurin <tkukurin@gmail.com>
2017-12-04 17:50:13 -0500

    added template url endpoint

--- djangorest/djangorest/urls.py
+++ djangorest/djangorest/urls.py
@@ -16,7 +16,15 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 
+from rest_framework.schemas import get_schema_view
+from rest_framework_swagger.renderers import SwaggerUIRenderer, OpenAPIRenderer
+
+# Django 3.5 ships with the 'get_schema_view' method. This method will generate a Django view for your schema.  
+# The method also allows us to pass in a 'Renderer' class.  A renderer will tell the view how it should render
+schema_view = get_schema_view(title='Users API', renderer_classes=[OpenAPIRenderer, SwaggerUIRenderer])
+
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-  	url(r'^', include('api.urls')),
+    url(r'^admin/', admin.site.urls), 
+    url(r'^', include('api.urls')),
+    url(r'^', schema_view, name="docs"),
 ]
